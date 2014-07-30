2ac2b3bbe7524cd7d1c5e3510c29db133278491b
Michael Stephens <michaelstephens1993@gmail.com>
2014-07-30 20:34:13 +0100

    more changes, but first working version

--- src/Scripter.hx
+++ src/Scripter.hx
@@ -1,5 +1,6 @@
 package;
 
+import cs.internal.HxObject;
 import haxe.Log;
 import hscript.Expr;
 import hscript.Interp;
@@ -15,6 +16,12 @@ extern class Action_0
 	function Invoke():Void;
 }
 
+@:native("System.Action")
+extern class Action_1<T>
+{
+	function Invoke(a:T):Void;
+}
+
 /**
  * ...
  * @author Michael Stephens
@@ -25,32 +32,27 @@ class Scripter
 	
 	var cScript:Expr;
 	
-	var scriptVars:Map<String, Dynamic>;
-
 	public function new(path_:String) 
 	{
 		var parser = new Parser();
 		
 		interp = new Interp();
 		
-		//cScript = parser.parseString(File.getContent(path_));
+		cScript = parser.parseString(File.getContent(path_));
 	}
-
-	/*public function AddFunction(name_:String, func_:Action_0):Void
-	{
-		//func_.Invoke();
-		scriptVars.set(name_, func_);
-	}*/
 	
-	public function AddFunction(name_:String, func_:cs.system.Action_1<String>):Void
+	public function AddVariable<T>(name_:String, variable_:T)
 	{
-		scriptVars.set(name_, func_.AsDelegate());
+		interp.variables[name_] = variable_;
+	}
+	
+	public function AddFunction<T>(name_:String, func_:Action_1<T>):Void
+	{
+		interp.variables[name_] = function(s) func_.Invoke(s);
 	}
 	
 	public function Run():Void
 	{
-		interp.variables = scriptVars;
-		
 		interp.execute(cScript);
 	}
 }
