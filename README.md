9cb362aedb9a6715882f188297e9904b0de4d62f
Michael Stephens <michaelstephens1993@gmail.com>
2014-08-06 12:14:34 +0100

    Works with Unity and standard CS

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ HaxeScript
 This project was created with the need for using a scripting language in Unity.
 Currently only compiles to CS but plan to add support for other languages.
 
+I did not create the scripter, i merely created an object for ease of use. The scripting repo is here: https://github.com/clemos/hscript
+
 Build out the DLL with the bat files for your relevant needs.
 
-I did not create the scripter, i merely created an object for ease of use. The scripting repo is here: https://github.com/clemos/hscript
+Unity - Must have Unity installed and have set environment path to the correct bin folder (mine is: 'C:\Program Files (x86)\Unity\Editor\Data\Mono\bin')
+
--- src/Scripter.hx
+++ src/Scripter.hx
@@ -1,6 +1,6 @@
 package;
 
-import cs.internal.Function.Closure;
+import cs.Lib;
 import haxe.Log;
 import hscript.Expr;
 import hscript.Interp;
@@ -15,36 +15,6 @@ extern class Action_0
 {
 	function Invoke():Void;
 }
-
-@:native("System.Action")
-extern class Action_1<T>
-{
-	function Invoke(t:T):Void;
-}
-
-@:native("System.Action")
-extern class Action_2<T, U>
-{
-	function Invoke(t:T, u:U):Void;
-}
-
-@:native("System.Action")
-extern class Action_3<T, U, V>
-{
-	function Invoke(t:T, u:U, v:V):Void;
-}
-
-@:native("System.Action")
-extern class Action_4<T, U, V, W>
-{
-	function Invoke(t:T, u:U, v:V, w:W):Void;
-}
-
-/*@:native("System.Action")
-extern class Action_5<T, U, V, W, X>
-{
-	function Invoke(t:T, u:U, v:V, w:W, x:X):Void;
-}*/
 
 /**
  * ...
@@ -65,45 +35,60 @@ class Scripter
 		cScript = parser.parseString(File.getContent(path_));
 	}
 	
-	public function Funky(name_:String, func_:Closure):Void
-	{
-		interp.variables[name_] = func_;
-	}
-	
 	public function AddVariable<T>(name_:String, variable_:T)
 	{
 		interp.variables[name_] = variable_;
 	}
 	
-	public function AddFunction(name_:String, func_:Action_0):Void
+	public function AddFunction<T>(name_:String, func_:cs.system.Func_1<T>):Void
+	{
+		interp.variables[name_] = function() return func_.Invoke();
+	}
+	
+	public function AddFunction1<T, U>(name_:String, func_:cs.system.Func_2<T, U>):Void
+	{
+		interp.variables[name_] = function(t) return func_.Invoke(t);
+	}
+	
+	public function AddFunction2<T, U, V>(name_:String, func_:cs.system.Func_3<T, U, V>):Void
+	{
+		interp.variables[name_] = function(t, u) return func_.Invoke(t, u);
+	}
+	
+	public function AddFunction3<T, U, V, W>(name_:String, func_:cs.system.Func_4<T, U, V, W>):Void
+	{
+		interp.variables[name_] = function(t, u, v) return func_.Invoke(t, u, v);
+	}
+	
+	public function AddFunction4<T, U, V, W, X>(name_:String, func_:cs.system.Func_5<T, U, V, W, X>):Void
+	{
+		interp.variables[name_] = function(t, u, v, w) return func_.Invoke(t, u, v, w);
+	}
+	
+	public function AddAction(name_:String, func_:Action_0):Void
 	{
 		interp.variables[name_] = function() func_.Invoke();
 	}
 	
-	public function AddFunction1<T>(name_:String, func_:Action_1<T>):Void
+	public function AddAction1<T>(name_:String, func_:cs.system.Action_1<T>):Void
 	{
 		interp.variables[name_] = function(t) func_.Invoke(t);
 	}
 	
-	public function AddFunction2<T, U>(name_:String, func_:Action_2<T, U>):Void
+	public function AddAction2<T, U>(name_:String, func_:cs.system.Action_2<T, U>):Void
 	{
 		interp.variables[name_] = function(t, u) func_.Invoke(t, u);
 	}
 	
-	public function AddFunction3<T, U, V>(name_:String, func_:Action_3<T, U, V>):Void
+	public function AddAction3<T, U, V>(name_:String, func_:cs.system.Action_3<T, U, V>):Void
 	{
 		interp.variables[name_] = function(t, u, v) func_.Invoke(t, u, v);
 	}
 	
-	public function AddFunction4<T, U, V, W>(name_:String, func_:Action_4<T, U, V, W>):Void
+	public function AddAction4<T, U, V, W>(name_:String, func_:cs.system.Action_4<T, U, V, W>):Void
 	{
 		interp.variables[name_] = function(t, u, v, w) func_.Invoke(t, u, v, w);
 	}
-	
-	/*public function AddFunction5<T, U, V, W, X>(name_:String, func_:Action_5<T, U, V, W, X>):Void
-	{
-		interp.variables[name_] = function(t, u, v, w, x) func_.Invoke(t, u, v, w, x);
-	}*/
 	
 	public function Run():Void
 	{
